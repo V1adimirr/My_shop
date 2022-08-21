448f0f59ca478846ebeaa75ef6f913fbf25ddea7
Vladimir <vladimirpedenko936@gmail.com>
2022-08-21 21:19:46 +0600

    "Добавил данные в проект"

--- my_shop/shop/settings.py
+++ my_shop/shop/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
+    'webapp',
 ]
 
 MIDDLEWARE = [
@@ -105,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Bishkek'
 
 USE_I18N = True
 
